2f748e80a178eac62a70cc7d3d50ff985dfd4fef
Vladislav Timakov <jrmnnl@gmail.com>
2019-07-30 20:09:55 +0300

    Polish

--- src/VeeamTest.MultithreadGZip.Tests/CompressionAndDecompressionTests.cs
+++ src/VeeamTest.MultithreadGZip.Tests/CompressionAndDecompressionTests.cs
@@ -11,17 +11,16 @@ namespace VeeamTest.MultithreadGZip.Tests
         {
             var expectedBytes = Helpers.GenerateRandomBytes(1000000);
 
-            var compressor = new Compressor(10000, 1);
-            var decompressor = new Decompressor(1);
-
             using (var original = new MemoryStream(expectedBytes))
             using (var compressed = new MemoryStream())
             using (var decompressed = new MemoryStream())
             {
+                var compressor = new Compressor(10000, 1);
                 compressor.Compress(original, compressed);
 
                 compressed.Seek(0, SeekOrigin.Begin);
 
+                var decompressor = new Decompressor(1);
                 decompressor.Decompress(compressed, decompressed);
 
                 CollectionAssert.AreEqual(expectedBytes, decompressed.ToArray());
